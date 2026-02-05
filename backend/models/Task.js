67bd48e450b7a7da950ca2d506dba3bbae5a0b1b
Subodha Sandawaruni <pdssandawaruni@students.nsbm.ac.lk>
2026-02-05 14:26:02 +0530

    update Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  completed: {
-    type: Boolean,
-    default: false
-  }
-});
+   const taskSchema = new mongoose.Schema({
+     title: String,
+     description: String,
+     completed: {
+       type: Boolean,
+       default: false
+     }
+   });
 
-module.exports = mongoose.model("Task", taskSchema);
+   module.exports = mongoose.model("Task", taskSchema);
